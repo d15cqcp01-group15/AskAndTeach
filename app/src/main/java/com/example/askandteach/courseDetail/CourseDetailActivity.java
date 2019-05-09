eda644c63826d160545fada684b590e7e98ba889
tailouis <vuductaiptit@gmail.com>
2019-05-09 14:14:16 +0700

    feature: register course

--- app/src/main/java/com/example/askandteach/courseDetail/CourseDetailActivity.java
+++ app/src/main/java/com/example/askandteach/courseDetail/CourseDetailActivity.java
@@ -7,28 +7,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.askandteach.AsyncTaskLoadImage;
+import com.example.askandteach.MainActivity;
 import com.example.askandteach.R;
 import com.example.askandteach.models.Course;
+import com.example.askandteach.models.RegisterCourse;
+import com.example.askandteach.models.RegisterCourseResp;
+import com.example.askandteach.retrofit.APIInterface;
+import com.example.askandteach.retrofit.RetrofitInstance;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CourseDetailActivity extends AppCompatActivity {
 
-    TextView tvName, price, time, skill, district, city, description;
+    TextView tvName, price, time, skill, district, city, description, tvNumberStudent;
     Button btnRegisterCourse;
+    String course_id;
+    ImageView avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_detail);
 
-        initial();
-        addEvents();
         addControls();
+        addEvents();
     }
 
-    private void initial(){
+    private void addControls(){
         tvName = findViewById(R.id.tvName);
         price = findViewById(R.id.tvPriceClassDetail);
         time = findViewById(R.id.tvTimeClassDetail);
@@ -36,9 +50,14 @@ public class CourseDetailActivity extends AppCompatActivity {
         district = findViewById(R.id.tvDistrictClassDetail);
         city = findViewById(R.id.tvCityClassDetail);
         description = findViewById(R.id.tvDescriptionClassDetail);
+        tvNumberStudent = findViewById(R.id.txtNumberStudent);
         btnRegisterCourse = findViewById(R.id.btnRegisterCourse);
+        avatar = findViewById(R.id.ivCourse);
 
         Course course = (Course) getIntent().getSerializableExtra("COURSE");
+
+        new AsyncTaskLoadImage(avatar).execute(course.getCoverImage());
+        course_id = course.getId().toString();
         price.setText(course.getPrice().toString());
         tvName.setText(course.getUser().getUsername());
         time.setText(course.getUptime());
@@ -46,19 +65,32 @@ public class CourseDetailActivity extends AppCompatActivity {
         district.setText(course.getDistrict());
         city.setText(course.getCity());
         description.setText(course.getDescription());
-    }
-
-    private void addControls(){
+        tvNumberStudent.setText(course.getAmountStudent().toString());
 
     }
 
     private void addEvents(){
+
         btnRegisterCourse.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO check authentication
-                //Send request register course
-                Toast.makeText(getApplicationContext(), "111111", Toast.LENGTH_LONG).show();
+                APIInterface service = RetrofitInstance.getRetrofitInstance().create(APIInterface.class);
+                RegisterCourse res = new RegisterCourse();
+                res.setCourseId(course_id);
+
+                Call<RegisterCourseResp> call = service.registerCourse(MainActivity.getTokenValue(), res);
+
+                call.enqueue(new Callback<RegisterCourseResp>() {
+                    @Override
+                    public void onResponse(Call<RegisterCourseResp> call, Response<RegisterCourseResp> response) {
+                        Toast.makeText(getApplicationContext(), "111", Toast.LENGTH_SHORT);
+                    }
+
+                    @Override
+                    public void onFailure(Call<RegisterCourseResp> call, Throwable t) {
+                        Toast.makeText(getApplicationContext(), "111", Toast.LENGTH_SHORT);
+                    }
+                });
             }
         });
     }
